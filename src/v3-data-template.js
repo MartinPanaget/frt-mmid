cc4ccf65b43109bd52450361ae284e44ccf82c95
katathering <57351169+katathering@users.noreply.github.com>
2022-01-24 22:17:41 +0100

    data template V3 and V4 added

---
+++ src/v3-data-template.js
@@ -0,0 +1,348 @@
+// V3: PERCENTAGE STACKED BAR CHART
+
+// ANMERKUNG
+// 'showInLegend' und nicht beachten/so lassen
+
+// ERKLÄRUNG (Attribute zum Anpassen)
+// name : erscheint beim Tooltip
+// color: Farbe des Balken-Abschnitts der dazugehörigen Datagroup (in unserem Fall die Farbe des Landes in verschiedenen Sättigungen)
+// data: der tatsächliche Wert in eckigen Klammern
+
+// WICHTIG
+// Es gibt die Gruppen: Convenience, Efficiency, Security, PrivacyViolation, Discrimination und Surveillance.
+// Innerhalb dieser Gruppen gibt es immer 1 Datensatz mit 4 Werte-Farbe-Kombos (je für ein Land) 
+// und 3 Datagroups ('Strongly Accept', 'Middle', 'Strongly Oppose' --> in dieser Reihenfolge).
+// Bedeutet: 
+    // 1. Die erste Datagroup ( hier: 'Strongly Accept') definiert immer den äußersten Wert über die vier Balken (Datensätze) hinweg.
+    //      Die letzte Datagroup den untersten Wert.
+    // 2. innderhalb des 'data'-Attributs gehört der erste Wert zum Balken ganz links (in unserem Fall China), 
+    //      der zweite gehört dem zweiten Balken von links (England) usw.
+    //      ACHTUNG: Wenn also die Reihenfolge der x achse geändert wird, müssen die Werte analog angepasst werden
+
+series:{
+    Convenience:[
+        { //start Datagroup
+        showInLegend: false,
+        visible: true,
+        name: 'Strongly Accept', //Tooltip
+        data: [{ //start Datensatz
+            y: 2, // Wert 'Strongly accept' CH
+            color: '#ff0000' // höchste Sättigung
+            }, {
+            y: 3,// Wert 'Strongly accept' GB
+            color: '#00ff00' // höchste Sättigung
+            },{
+            y: 3,// Wert 'Strongly accept' USA
+            color: '#00ff00' // höchste Sättigung
+            },{
+            y: 1,// Wert 'Strongly accept' DE
+            color: '#0000ff' // höchste Sättigung
+            }
+        ] //ende Datensatz
+    }, //ende Datagroup
+    {
+        name: 'Middle', //Tooltip
+        data: [{
+            y: 2, // Wert 'middle' CH
+            color: '#ff0000' // mittlere Sättigung
+            }, {
+            y: 3,// Wert 'middle' GB
+            color: '#00ff00' // mittlere Sättigung
+            },{
+            y: 3,// Wert 'middle' USA
+            color: '#00ff00' // mittlere Sättigung
+            },{
+            y: 1,// Wert 'middle' DE
+            color: '#0000ff' // mittlere Sättigung
+            }
+        ]
+    },
+    {
+        name: 'Strongly Oppose', //Tooltip
+        data: [{
+            y: 2, // Wert 'Strongly Oppose' CH
+            color: '#ff0000' // geringste Sättigung
+            }, {
+            y: 3,// Wert 'Strongly Oppose' GB
+            color: '#00ff00' // geringste Sättigung
+            },{
+            y: 3,// Wert 'Strongly Oppose' USA
+            color: '#00ff00' // geringste Sättigung
+            },{
+            y: 1,// Wert 'Strongly Oppose' DE
+            color: '#0000ff' // geringste Sättigung
+            }
+        ]
+
+    }];
+    Efficiency:[{ 
+        showInLegend: false,
+        visible: true,
+        name: 'Strongly Accept', 
+        data: [{
+            y: 2, 
+            color: '#ff0000' 
+            }, {
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 1,
+            color: '#0000ff' 
+            }
+        ]
+    },
+    {
+        name: 'Middle', 
+        data: [{
+            y: 2, 
+            color: '#ff0000' 
+            }, {
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 1,
+            color: '#0000ff' 
+            }
+        ]
+    },
+    {
+        name: 'Strongly Oppose', 
+        data: [{
+            y: 2, 
+            color: '#ff0000' 
+            }, {
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 1,
+            color: '#0000ff'
+            }
+        ]
+
+    }];
+    Security:[{ 
+        showInLegend: false,
+        visible: true,
+        name: 'Strongly Accept', 
+        data: [{
+            y: 2, 
+            color: '#ff0000' 
+            }, {
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 1,
+            color: '#0000ff' 
+            }
+        ]
+    },
+    {
+        name: 'Middle',
+        data: [{
+            y: 2, 
+            color: '#ff0000' 
+            }, {
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 1,
+            color: '#0000ff' 
+            }
+        ]
+    },
+    {
+        name: 'Strongly Oppose', 
+        data: [{
+            y: 2, 
+            color: '#ff0000' 
+            }, {
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 1,
+            color: '#0000ff' 
+            }
+        ]
+
+    }];
+    PrivacyViolation:[{ 
+        showInLegend: false,
+        visible: true,
+        name: 'Strongly Accept', 
+        data: [{
+            y: 2, 
+            color: '#ff0000' 
+            }, {
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 1,
+            color: '#0000ff' 
+            }
+        ]
+    },
+    {
+        name: 'Middle', 
+        data: [{
+            y: 2, 
+            color: '#ff0000' 
+            }, {
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 1,
+            color: '#0000ff' 
+            }
+        ]
+    },
+    {
+        name: 'Strongly Oppose', 
+        data: [{
+            y: 2, 
+            color: '#ff0000' 
+            }, {
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 1,
+            color: '#0000ff' 
+            }
+        ]
+
+    }];
+    Discrimination:[{ 
+        showInLegend: false,
+        visible: true,
+        name: 'Strongly Accept', 
+        data: [{
+            y: 2, 
+            color: '#ff0000' 
+            }, {
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 1,
+            color: '#0000ff' 
+            }
+        ]
+    },
+    {
+        name: 'Middle', 
+        data: [{
+            y: 2, 
+            color: '#ff0000' 
+            }, {
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 1,
+            color: '#0000ff' 
+            }
+        ]
+    },
+    {
+        name: 'Strongly Oppose',
+        data: [{
+            y: 2, 
+            color: '#ff0000' 
+            }, {
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 1,
+            color: '#0000ff' 
+            }
+        ]
+
+    }];
+    Surveillance:[{ 
+        showInLegend: false,
+        visible: true,
+        name: 'Strongly Accept',
+        data: [{
+            y: 2, 
+            color: '#ff0000' 
+            }, {
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 1,
+            color: '#0000ff' 
+            }
+        ]
+    },
+    {
+        name: 'Middle', 
+        data: [{
+            y: 2, 
+            color: '#ff0000' 
+            }, {
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 1,
+            color: '#0000ff' 
+            }
+        ]
+    },
+    {
+        name: 'Strongly Oppose', 
+        data: [{
+            y: 2, 
+            color: '#ff0000' 
+            }, {
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 3,
+            color: '#00ff00' 
+            },{
+            y: 1,
+            color: '#0000ff' 
+            }
+        ]
+
+    }]
+}
---
+++ src/v4-data-tempalte.js
@@ -0,0 +1,313 @@
+// V4: BUBBLECHART
+
+// ANMERKUNG
+// 'showInLegend' und 'visible' nicht beachten/so lassen
+
+// ERKLÄRUNG (Attribute zum Anpassen)
+// name : erscheint beim Tooltip
+// color: Farbe der Bubble der dazugehörigen Datagroup (in unserem Fall die Farbe des Landes)
+// data: der tatsächliche Wert in eckigen Klammern
+
+// WICHTIG
+// Es gibt die Gruppen: Phone, PersonCheck, SmartDevice, IdCheck, Transport, Mall, Street, School, Private und Tourism.
+// Innerhalb dieser Gruppen gibt es immer 4 Datagroups (also je eine für ein Land)
+// Die Reihenfolge dieser Datagroups ist wichtig.
+// Bedeutet: Die erste Datagroup muss den Datensatz mit dem kleinsten Wert besitzen, 
+// die letzte (also vierte) den Datensatz mit dem größten Wert.
+
+
+
+series:{
+    Phone:[//Gruppenname
+        { //start Datagroup
+        showInLegend: false,
+        visible: true,
+        name: 'Phone DE', //Tooltip
+        color: '#23EFEF', //Landfarbe
+        data:[12],// Datensatz mit kleinestem Wert in der Gruppe Phone
+        },//Ende Datagroup
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone GB',
+        color: '#70E6AB',
+        data:[20],
+        }, 
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone USA',
+        color: '#0CBAF7',
+        data:[25],
+        },
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone CH',
+        color: '#C7F700',
+        data:[50],// Datensatz mit größtem Wert in der Gruppe Phone
+        }
+    ]; // Ende erste Gruppe 
+    PersonCheck:[{
+        showInLegend: false,
+        visible: true,
+        name: 'Phone DE',
+        color: '#23EFEF',
+        data:[12],
+        },
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone GB',
+        color: '#70E6AB',
+        data:[20],
+        },
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone CH',
+        color: '#C7F700',
+        data:[25],
+        },
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone USA',
+        color: '#0CBAF7',
+        data:[50],
+        }
+    ];
+    SmartDevice:[{
+        showInLegend: false,
+        visible: true,
+        name: 'Phone DE',
+        color: '#23EFEF',
+        data:[12],
+        },
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone GB',
+        color: '#70E6AB',
+        data:[20],
+        },
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone USA',
+        color: '#0CBAF7',
+        data:[25],
+        },
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone CH',
+        color: '#C7F700',
+        data:[30],
+        }
+    ];
+    IdCheck:[{
+        showInLegend: false,
+        visible: true,
+        name: 'Phone DE',
+        color: '#23EFEF',
+        data:[12],
+        },
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone GB',
+        color: '#70E6AB',
+        data:[20],
+        },
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone USA',
+        color: '#0CBAF7',
+        data:[25],
+        },
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone CH',
+        color: '#C7F700',
+        data:[30],
+        }
+    ];
+    Transport:[{
+        showInLegend: false,
+        visible: true,
+        name: 'Phone DE',
+        color: '#23EFEF',
+        data:[12],
+        },
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone GB',
+        color: '#70E6AB',
+        data:[20],
+        },
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone USA',
+        color: '#0CBAF7',
+        data:[25],
+        },
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone CH',
+        color: '#C7F700',
+        data:[30],
+        }
+    ];
+    Mall:[{
+        showInLegend: false,
+        visible: true,
+        name: 'Phone DE',
+        color: '#23EFEF',
+        data:[12],
+        },
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone GB',
+        color: '#70E6AB',
+        data:[20],
+        },
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone USA',
+        color: '#0CBAF7',
+        data:[25],
+        },
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone CH',
+        color: '#C7F700',
+        data:[30],
+        }
+    ];
+    Street:[{
+        showInLegend: false,
+        visible: true,
+        name: 'Phone DE',
+        color: '#23EFEF',
+        data:[12],
+        },
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone GB',
+        color: '#70E6AB',
+        data:[20],
+        },
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone USA',
+        color: '#0CBAF7',
+        data:[25],
+        },
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone CH',
+        color: '#C7F700',
+        data:[30],
+        }
+    ];
+    School:[{
+        showInLegend: false,
+        visible: true,
+        name: 'Phone DE',
+        color: '#23EFEF',
+        data:[12],
+        },
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone GB',
+        color: '#70E6AB',
+        data:[20],
+        },
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone USA',
+        color: '#0CBAF7',
+        data:[25],
+        },
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone CH',
+        color: '#C7F700',
+        data:[30],
+        }
+    ];
+    Private:[{
+        showInLegend: false,
+        visible: true,
+        name: 'Phone DE',
+        color: '#23EFEF',
+        data:[12],
+        },
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone CH',
+        color: '#C7F700',
+        data:[20],
+        },
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone USA',
+        color: '#0CBAF7',
+        data:[25],
+        },
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone GB',
+        color: '#70E6AB',
+        data:[30],
+        }
+    ];
+    Tourism:[{
+        showInLegend: false,
+        visible: true,
+        name: 'Phone CH',
+        color: '#C7F700',
+        data:[12],
+        },
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone GB',
+        color: '#70E6AB',
+        data:[20],
+        },
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone USA',
+        color: '#0CBAF7',
+        data:[25],
+        },
+        {
+        showInLegend: false,
+        visible: true,
+        name: 'Phone DE',
+        color: '#23EFEF',
+        data:[80],
+        }
+    ];
+    
+}
